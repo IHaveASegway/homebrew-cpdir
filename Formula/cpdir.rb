abe53015b699f2d4d18cedbe47901b8aa652a6bd
Joseph Peterson <joseph.peterson@goaptive.com>
2025-04-22 13:29:50 -0600

    Add initial formula for cpdir to Homebrew

---
+++ Formula/cpdir.rb
@@ -0,0 +1,21 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Cpdir < Formula
+    desc "Copy specific files and the directory structure to the clipboard"
+    homepage "https://github.com/IHaveASegway/cpdir"
+    url "https://github.com/IHaveASegway/cpdir/archive/refs/tags/v1.0.0.tar.gz"
+    sha256 "4c3ed5dd6f4157c2862878cd297f4ae547f32a0dc1fcd8bd50828f0fd6b28c03"
+    license "MIT"
+  
+    depends_on "go" => :build
+  
+    def install
+      system "go", "build", "-o", bin/"cpdir", "."
+    end
+  
+    test do
+      assert_match "Usage", shell_output("#{bin}/cpdir --help")
+    end
+  end
+  
